fe94d5f996b2d21939c2f8db4e29ba0e80d55fc7
Assam Toumi <assamtomi@outlook.com>
2020-07-03 21:16:55 +0100

    done!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeFirstWithDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
